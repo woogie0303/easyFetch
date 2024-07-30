c3e1d2cd5765b50fdec64bf2e54b5f4c181a50f1
woogie0303 <rkdehddnr15123@gmail.com>
2024-07-30 17:08:09 +0900

    test: next 프로퍼티 추가시 request instance와 합치는 테스트 추가

--- test/index.test.ts
+++ test/index.test.ts
@@ -45,4 +45,44 @@ describe('EasyFetch', () => {
       window: undefined,
     });
   });
+
+  it('merges requestInstance when requestInit with a next property is passed as an argument', async () => {
+    // given
+    const spyRequestMethod = vi.spyOn(RequestUtils, 'mergeRequestConfig');
+    const easy = easyFetch();
+
+    // then
+    const request = new Request('http://sdf', {
+      mode: 'cors',
+      cache: 'no-cache',
+    });
+    await easy.request(request, {
+      next: { revalidate: 300 },
+    });
+
+    // then
+    const expectRequestInstance = new Request('http://sd', {
+      cache: 'no-cache',
+    });
+    const expectRequestBody = await expectRequestInstance.arrayBuffer();
+    const actualRequestValue = await spyRequestMethod.mock.results[0].value;
+
+    expect(actualRequestValue[1]).toStrictEqual({
+      body: expectRequestBody,
+      cache: expectRequestInstance.cache,
+      credentials: expectRequestInstance.credentials,
+      headers: expectRequestInstance.headers,
+      integrity: expectRequestInstance.integrity,
+      keepalive: expectRequestInstance.keepalive,
+      method: expectRequestInstance.method,
+      mode: expectRequestInstance.mode,
+      priority: undefined,
+      referrer: expectRequestInstance.referrer,
+      redirect: expectRequestInstance.redirect,
+      referrerPolicy: expectRequestInstance.referrerPolicy,
+      signal: expectRequestInstance.signal,
+      window: undefined,
+      next: { revalidate: 300 },
+    });
+  });
 });
